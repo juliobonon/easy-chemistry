5ca9f7e2fbdd8afaa9ff07482012843774e07984
Julio Cesar <juliocesarbonon@hotmail.com>
2020-02-18 23:16:25 -0300

    Fix interfaces and add math logic

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,29 @@ class LiquidoCombinadoWindow extends StatefulWidget {
 }
 
 class _LiquidoCombinadoWindowState extends State<LiquidoCombinadoWindow> {
+  TextEditingController massaController = TextEditingController();
+  TextEditingController absController = TextEditingController();
+
+  String _infotext = "Coloque sua variavel na conta acima";
+
+  void _calculate() {
+    setState(() {
+      double _massaValue = double.parse(massaController.text);
+      double _absValue = double.parse(absController.text);
+
+      double result = _absValue * 100 * 25 / 0.2 + _massaValue + 2870;
+
+      _infotext = "Seu resultado é (${result.toStringAsPrecision(4)})";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Liquido Combinado"),
         centerTitle: true,
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.green[900],
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(30),
@@ -27,16 +43,30 @@ class _LiquidoCombinadoWindowState extends State<LiquidoCombinadoWindow> {
                 child: Image(
                   image: AssetImage('imgs/flask.png'),
                 )),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                decoration: InputDecoration(hintText: 'Digite a variavel'),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(hintText: 'ABS'),
+                    controller: absController,
+                  ),
+                ),
+                Spacer(),
+                Flexible(
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(hintText: 'Massa'),
+                    controller: massaController,
+                  ),
+                )
+              ],
             ),
             Padding(
               padding: EdgeInsets.all(10),
               child: Text(
-                ("(Valor da titulação x 56,11 x 100 ) / 20.000"),
+                ("(( (ABS x 100 x 25) ÷ 0,2) ÷ Massa) ÷ 2870"),
                 style: TextStyle(color: Colors.blueGrey, fontSize: 15.0),
               ),
             ),
@@ -45,9 +75,15 @@ class _LiquidoCombinadoWindowState extends State<LiquidoCombinadoWindow> {
               child: RaisedButton(
                 color: Colors.black45,
                 child: Text("Make it rain!",
-                    style: TextStyle(color: Colors.blue, fontSize: 25.0)),
-                onPressed: () {},
+                    style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                onPressed: () {
+                  _calculate();
+                },
               ),
+            ),
+            Text(
+              _infotext,
+              style: TextStyle(color: Colors.black38, fontSize: 20.0),
             )
           ],
         ),
@@ -62,13 +98,27 @@ class NorbixinaWindow extends StatefulWidget {
 }
 
 class _NorbixinaWindowState extends State<NorbixinaWindow> {
+  TextEditingController norbixinaController = TextEditingController();
+
+  String _texto = "Coloque sua variavel na conta acima";
+
+  void _calculate() {
+    setState(() {
+      double _value = double.parse(norbixinaController.text);
+
+      double result = _value * 56.11 * 100 / 20.000;
+
+      _texto = "Seu resultado é (${result.toStringAsPrecision(4)})";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Norbixina"),
         centerTitle: true,
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.green[900],
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(30),
@@ -82,8 +132,9 @@ class _NorbixinaWindowState extends State<NorbixinaWindow> {
             Padding(
               padding: EdgeInsets.all(10),
               child: TextField(
-                decoration: InputDecoration(hintText: 'Digite a variavel'),
-              ),
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(hintText: 'Digite a variavel'),
+                  controller: norbixinaController),
             ),
             Padding(
               padding: EdgeInsets.all(10),
@@ -97,10 +148,17 @@ class _NorbixinaWindowState extends State<NorbixinaWindow> {
               child: RaisedButton(
                 color: Colors.black38,
                 child: Text("Make it rain!",
-                    style: TextStyle(color: Colors.yellow, fontSize: 25.0)),
-                onPressed: () {},
+                    style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                onPressed: () {
+                  _calculate();
+                },
               ),
-            )
+            ),
+            Center(
+                child: Text(
+              _texto,
+              style: TextStyle(color: Colors.black38, fontSize: 20.0),
+            ))
           ],
         ),
       ),
@@ -116,21 +174,27 @@ class KOHWindow extends StatefulWidget {
 class _KOHWindowState extends State<KOHWindow> {
   TextEditingController k0hController = TextEditingController();
 
-
   String _texto = "Coloque sua variavel na conta acima";
 
-  void _calculate(){
+  void _calculate() {
     setState(() {
-    double _value = double.parse(k0hController.text);
+      double _value = double.parse(k0hController.text);
 
-    double result =  _value * 56.11 * 100 / 20.000 *10;
+      double result = _value * 56.11 * 100 / 20.000 * 10;
 
-    _texto = "Seu resultado é (${result.toStringAsPrecision(4)})";
+      _texto = "Seu resultado é (${result.toStringAsPrecision(4)})";
     });
-    
-
   }
 
+  void _calculate40() {
+    setState(() {
+      double _value = double.parse(k0hController.text);
+
+      double result = _value * 56.11 * 100 / 20.000 * 20;
+
+      _texto = "Seu resultado é (${result.toStringAsPrecision(4)})";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -138,7 +202,7 @@ class _KOHWindowState extends State<KOHWindow> {
       appBar: AppBar(
         title: Text("KOH"),
         centerTitle: true,
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.green[900],
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(30),
@@ -152,6 +216,7 @@ class _KOHWindowState extends State<KOHWindow> {
             Padding(
               padding: EdgeInsets.all(10),
               child: TextField(
+                textAlign: TextAlign.center,
                 controller: k0hController,
                 decoration: InputDecoration(hintText: 'Digite a variavel'),
               ),
@@ -168,24 +233,31 @@ class _KOHWindowState extends State<KOHWindow> {
               children: <Widget>[
                 RaisedButton(
                   color: Colors.black38,
-                  child: Text("20%",
-                  style: TextStyle(color: Colors.white),),
+                  child: Text(
+                    "20%",
+                    style: TextStyle(color: Colors.white),
+                  ),
                   onPressed: () {
                     _calculate();
                   },
                 ),
-              RaisedButton(
-                color: Colors.black38,
-                child: Text("40%",
-                style: TextStyle(color: Colors.white),),
-                onPressed: (){},
-              ),
+                RaisedButton(
+                  color: Colors.black38,
+                  child: Text(
+                    "40%",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    _calculate40();
+                  },
+                ),
               ],
             ),
             Text(
-                _texto,
-                textAlign: TextAlign.center,
-              )
+              _texto,
+              style: TextStyle(color: Colors.black38, fontSize: 20.0),
+              textAlign: TextAlign.center,
+            )
           ],
         ),
       ),
@@ -205,7 +277,7 @@ class _HomeState extends State<Home> {
         appBar: AppBar(
           title: Text("Easy Chemistry"),
           centerTitle: true,
-          backgroundColor: Colors.pink,
+          backgroundColor: Colors.green[900],
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.refresh),
@@ -216,9 +288,9 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.white,
         body: Center(
             child: Container(
-          margin: const EdgeInsets.all(25.0),
+          margin: const EdgeInsets.all(20.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 50.0),
@@ -226,48 +298,100 @@ class _HomeState extends State<Home> {
                   image: AssetImage('imgs/hansen.jpg'),
                 ),
               ),
-              Container(
-                child:            RaisedButton(
-                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: Text(
-                  "Norbixina",
-                  style: TextStyle(color: Colors.pink, fontSize: 25.0),
+              Flexible(
+                child: FractionallySizedBox(
+                    widthFactor: 1,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white),
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Colors.lightBlue[900],
+                              Colors.lightBlue[600]
+                            ],
+                          )),
+                      padding: EdgeInsets.all(20.0),
+                      child: FlatButton(
+                        child: Text(
+                          "Norbixina",
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => NorbixinaWindow()),
+                          );
+                        },
+                      ),
+                    )),
+              ),
+              Flexible(
+                child: FractionallySizedBox(
+                  widthFactor: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(10),
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.lightBlue[900],
+                            Colors.lightBlue[600]
+                          ],
+                        )),
+                    padding: EdgeInsets.all(20),
+                    child: FlatButton(
+                      child: Text(
+                        "K0H",
+                        style: TextStyle(color: Colors.white, fontSize: 25.0),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => KOHWindow()),
+                        );
+                      },
+                    ),
+                  ),
                 ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => NorbixinaWindow()),
-                  );
-                },
               ),
-              ),
-              
-              RaisedButton(
-                padding: EdgeInsets.fromLTRB(37, 0, 37, 0),
-                child: Text(
-                  "K0H",
-                  style: TextStyle(color: Colors.pink, fontSize: 25.0),
+              Flexible(
+                child: FractionallySizedBox(
+                  widthFactor: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(10),
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.lightBlue[900],
+                            Colors.lightBlue[600]
+                          ],
+                        )),
+                    padding: EdgeInsets.all(20),
+                    child: FlatButton(
+                      child: Text(
+                        "Liquido Combinado",
+                        style: TextStyle(color: Colors.white, fontSize: 25.0),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LiquidoCombinadoWindow()),
+                        );
+                      },
+                    ),
+                  ),
                 ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => KOHWindow()),
-                  );
-                },
-              ),
-              RaisedButton(
-                padding: EdgeInsets.fromLTRB(27, 0, 27, 0),
-                child: Text(
-                  "Liquido Combinado",
-                  style: TextStyle(color: Colors.pink, fontSize: 25.0),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) =>LiquidoCombinadoWindow()),
-                  );
-                },
-              ),
+              )
             ],
           ),
         )));
